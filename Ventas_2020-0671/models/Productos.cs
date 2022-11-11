60ff152a5f77b979f6e896b9dba6290a370d71ce
Chacho0 <117244336+Chacho0@users.noreply.github.com>
2022-11-11 18:14:39 -0400

    Add files via upload

---
+++ Ventas_2020-0671/models/Productos.cs
@@ -0,0 +1,10 @@
+namespace models; 
+
+public class producto
+{
+    public int productoID {get; set;}
+     public string Nombre {get; set;}
+     public string? Descripcio {get; set;}
+     public string Precio {get; set;}
+
+}
